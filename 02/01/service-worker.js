34396b418232faad6a0a1f9816a0d88f0928c2b0
Daijiro Wachi <daijiro.wachi@gmail.com>
2016-04-04 01:11:27 +0200

    add 02/01

---
+++ 02/01/service-worker.js
@@ -0,0 +1,58 @@
+'use strict';
+
+var version = 1;
+var currentCache = {
+  offline: 'offline-cache' + version
+};
+
+var offlineUrl = 'offline.html';
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(currentCache.offline).then(function(cache) {
+      return cache.addAll([
+        'offline.svg',
+        offlineUrl
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  var request = event.request,
+    headerIncludesHTML = request.headers.get('accept').includes('text/html'),
+    isRequestMethodGET = request.method === 'GET';
+
+  if (request.mode === 'navigate' || isRequestMethodGET) {
+    event.respondWith(
+      fetch(createRequestWithCacheBusting(event.request.url)).catch(function(error) {
+        console.log('OFFLINE: Returning offline page.', error);
+        return caches.match(offlineUrl);
+      })
+    );
+  } else {
+    event.respondWith(caches.match(event.request)
+        .then(function (response) {
+        return response || fetch(event.request);
+      })
+    );
+  }
+});
+
+function createRequestWithCacheBusting(url) {
+  var request,
+    cacheBustingUrl;
+
+  request = new Request(url,
+    {cache: 'reload'}
+  );
+
+  if ('cache' in request) {
+    return request;
+  }
+
+  cacheBustingUrl = new URL(url, self.location.href);
+  cacheBustingUrl.search += (cacheBustingUrl.search ? '&' : '') + 'cachebust=' + Date.now();
+
+  return new Request(cacheBustingUrl);
+}
